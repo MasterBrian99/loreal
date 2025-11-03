4f2448c4a904b2739531fefcdbfc454aec6859ab
Pasindu Pramodya <git@pasindupramodya.com>
2025-11-03 05:55:34 +0530

    feat: add Statement, Pattern, Type, and MatchArm types to loreal_ast

--- loreal_ast/src/lib.rs
+++ loreal_ast/src/lib.rs
@@ -37,6 +37,78 @@ pub enum UnOp {
     Not,
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum Statement {
+    Let {
+        pattern: Pattern,
+        type_annotation: Option<Type>,
+        value: Expr,
+        span: Span,
+    },
+    Expr { expr: Expr, span: Span },
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Pattern {
+    Wildcard { span: Span },
+    Identifier { name: SmolStr, span: Span },
+    Literal { expr: Expr, span: Span },
+    Tuple { patterns: Vec<Pattern>, span: Span },
+    Struct {
+        name: SmolStr,
+        fields: Vec<(SmolStr, Pattern)>,
+        span: Span,
+    },
+}
+
+impl Pattern {
+    pub fn span(&self) -> Span {
+        match self {
+            Pattern::Wildcard { span } => *span,
+            Pattern::Identifier { span, .. } => *span,
+            Pattern::Literal { expr, .. } => expr.span(),
+            Pattern::Tuple { span, .. } => *span,
+            Pattern::Struct { span, .. } => *span,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Type {
+    Named { name: SmolStr, span: Span },
+    Function {
+        params: Vec<Type>,
+        return_type: Box<Type>,
+        span: Span,
+    },
+    List { element_type: Box<Type>, span: Span },
+    Tuple { types: Vec<Type>, span: Span },
+    Map {
+        key_type: Box<Type>,
+        value_type: Box<Type>,
+        span: Span,
+    },
+}
+
+impl Type {
+    pub fn span(&self) -> Span {
+        match self {
+            Type::Named { span, .. } => *span,
+            Type::Function { span, .. } => *span,
+            Type::List { span, .. } => *span,
+            Type::Tuple { span, .. } => *span,
+            Type::Map { span, .. } => *span,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct MatchArm {
+    pub pattern: Pattern,
+    pub guard: Option<Expr>,
+    pub body: Expr,
+    pub span: Span,
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Declaration {
