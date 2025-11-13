9f767bcb8d533a7439df8f736acadee64e174a73
Pasindu Pramodya <git@pasindupramodya.com>
2025-11-14 03:42:21 +0530

    feat: add CodegenError and stub methods to CodeGenerator

--- loreal_codegen/src/lib.rs
+++ loreal_codegen/src/lib.rs
@@ -7,11 +7,27 @@ pub struct CodeGenerator {
     pub structs: Vec<StructDef>,
 }
 
+#[derive(Debug)]
+pub enum CodegenError {
+    #[error("Failed to generate code for function: {0}")]
+    GenerationError(String),
+}
+
+impl std::error::Error for CodegenError {}
+
 impl CodeGenerator {
     pub fn new(module_name: &str, structs: Vec<StructDef>) -> Self {
         Self {
             module_name: module_name.into(),
             structs,
         }
+    }
+
+    pub fn emit_obj(&self, functions: &[MirFunction], output: &str) -> Result<(), CodegenError> {
+        Err(CodegenError::GenerationError("Not implemented".into()))
+    }
+
+    pub fn emit_llvm_ir(&self, functions: &[MirFunction], output: &str) -> Result<(), CodegenError> {
+        Err(CodegenError::GenerationError("Not implemented".into()))
     }
 }
